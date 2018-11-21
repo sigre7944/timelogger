aedcd6898ea750299b0826aabd22fa42569c87ad
PRINCIPAL <principal@83-245-207-86.elisa-laajakaista.fi>
2018-11-21 23:26:37 +0200

    add endpoint

--- app.go
+++ app.go
@@ -2,39 +2,47 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	. "github.com/user/timelogger2/dao"
 	. "github.com/user/timelogger2/models"
+
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+//Init variable
+var timelogs []Timelog
+
 var dao = TimelogDAO{}
 
 func AllTimelogEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(timelogs)
 }
 
 func CreateTimelogEndPoint(w http.ResponseWriter, r *http.Request) {
-	// defer r.Body.Close()
-	// var timelog Timelog
-	// //decode the request body into a timelog object, assign it an ID, and use DAO insert to create a timelog in the database
-	// if err := json.NewDecoder(r.Body).Decode(&timelog); err != nil {
-	// 	respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-	// 	return
-	// }
-	// timelog.ID = bson.NewObjectId()
-	// if err := dao.Insert(timelog); err != nil {
-	// 	respondWithError(w, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
-	// respondWithJson(w, http.StatusCreated, timelog)
+	w.Header().Set("Content-Type", "application/json")
+	var timelog Timelog
+	_ = json.NewDecoder(r.Body).Decode(&timelog)
+	timelog.ID = strconv.Itoa(rand.Intn(1000000))
+	timelogs = append(timelogs, timelog)
+	json.NewEncoder(w).Encode(&Timelog(timelog))
 }
 
 func FindTimelogByDate(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get parameters
+
+	for _, item := range timelogs {
+		if item.Day == params["day"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Timelog{})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
@@ -50,9 +58,15 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 
 func main() {
 	r := mux.NewRouter()
+
+	// Mock Data -@todo -implement DB
+	timelogs = append(timelogs, Timelog{ID: "1", Day: "21", PointType: "start", Activity: "Play"})
+	timelogs = append(timelogs, Timelog{ID: "2", Day: "22", PointType: "stop", Activity: "Play"})
+
 	r.HandleFunc("/timelog", AllTimelogEndPoint).Methods("GET")
 	r.HandleFunc("/timelog", CreateTimelogEndPoint).Methods("POST")
 	r.HandleFunc("/timelog/{day}", FindTimelogByDate).Methods("GET")
+
 	if err := http.ListenAndServe(":3002", r); err != nil {
 		log.Fatal(err)
 	}
